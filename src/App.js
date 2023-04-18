e42b2b5e377512e0a0bca94d50fecf7fbd0a92fe
ngenieRonald <ingenieronaldcardenas@gmail.com>
2023-04-18 16:40:30 -0500

    hasta el video 7

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Header from './components/Header'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+
+const Contenedor = styled.div`
+ max-width: 600px;
+ margin: 0 auto;
+`
+const ContenedorFormulario = styled.div`
+ background-color: #fff;
+ padding: 3rem;
+`
+
+function App() {
+ return (
+  <Contenedor>
+   <Header titulo='Cotizador de Seguros' />
+   <ContenedorFormulario>
+    <Formulario />
+   </ContenedorFormulario>
+  </Contenedor>
+ )
+}
+
+export default App
